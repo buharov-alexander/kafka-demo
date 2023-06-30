40b78fa9977c8be82f2f6e34d98b6f4d2721dd89
Alexander Bukharov <buharovsasha@yandex.ru>
2023-07-01 01:37:00 +0700

    update layout demo1

--- app/src/main/kotlin/ru/buharov/kafka/service/ui/demo/ProviderAndConsumerDemo.kt
+++ app/src/main/kotlin/ru/buharov/kafka/service/ui/demo/ProviderAndConsumerDemo.kt
@@ -1,19 +1,24 @@
 package ru.buharov.kafka.service.ui.demo
 
 import com.vaadin.flow.component.html.Div
-import org.springframework.beans.factory.annotation.Value
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import ru.buharov.kafka.service.kafkaclient.KafkaService
 import ru.buharov.kafka.service.ui.component.MessageConsumer
 import ru.buharov.kafka.service.ui.component.MessageProvider
 
+
 class ProviderAndConsumerDemo(kafkaService: KafkaService) : Div() {
 
 	init {
-		val topic = kafkaService.getKafkaTopicDemo1Name()
-		add(MessageProvider("Provider 1") { value: String -> kafkaService.sendMessage(topic, value) })
-		val messageConsumer = MessageConsumer("Consumer 1")
-		add(messageConsumer)
+		val layout = HorizontalLayout()
+		layout.isPadding = true
 
+		val topic = kafkaService.getKafkaTopicDemo1Name()
+		layout.add(MessageProvider("Provider 1") { value: String -> kafkaService.sendMessage(topic, value) })
+		val messageConsumer = MessageConsumer("Consumer 1")
+		layout.add(messageConsumer)
+
+		add(layout)
 		kafkaService.registerMessageHandler(topic) { message: String ->
 			messageHandler(
 				message,
